13c7942d8f2856aea0d8a61446572ea5bf61fc83
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-12-02 22:38:00 +0100

    fix strip-docs

--- tempsdb/exceptions.pyx
+++ tempsdb/exceptions.pyx
@@ -1,18 +1,19 @@
 class TempsDBError(Exception):
     """Base class for TempsDB errors"""
-
+    ...
 
 class DoesNotExist(TempsDBError):
     """The required resource does not exist"""
-
+    ...
 
 class Corruption(TempsDBError):
     """Corruption was detected in the dataset"""
-
+    ...
 
 class InvalidState(TempsDBError):
     """An attempt was made to write to a resource that's closed"""
-
+    ...
 
 class AlreadyExists(TempsDBError):
     """Provided object already exists"""
+    ...
