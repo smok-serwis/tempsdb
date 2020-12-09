2f57c1e5c5cf240a3f0019f9a4b53a939bd89a61
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-12-09 17:57:12 +0100

    much faster compilation

--- setup.py
+++ setup.py
@@ -7,11 +7,14 @@ from distutils.core import setup
 
 from setuptools import Extension
 from snakehouse import Multibuild, build
+from satella.distutils import monkey_patch_parallel_compilation
 
 
 def find_pyx(*path) -> tp.List[str]:
     return list(find_files(os.path.join(*path), r'(.*)\.pyx', scan_subdirectories=True))
 
+
+monkey_patch_parallel_compilation()
 
 # extensions = [Extension("tempsdb.chunks", ['tempsdb/chunks.pyx']),
 #               Extension("tempsdb.database", ['tempsdb/database.pyx']),
@@ -27,7 +30,7 @@ if 'CI' in os.environ:
 setup(name='tempsdb',
       version='0.4.4a3',
       packages=['tempsdb'],
-      install_requires=['satella>=2.14.21', 'ujson'],
+      install_requires=['satella>=2.14.23', 'ujson'],
       ext_modules=build([Multibuild('tempsdb', find_pyx('tempsdb')), ],
                         compiler_directives=directives),
       # ext_modules=cythonize(extensions,
