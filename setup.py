c0c2c224410babfa5cbb3612415cb8f712bf3248
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-01-12 11:08:46 +0100

    0.5.4

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ ext_modules = build([Multibuild('tempsdb', find_pyx('tempsdb'),
                      **cythonize_kwargs)
 
 setup(name='tempsdb',
-      version='0.5.4b1',
+      version='0.5.4',
       packages=find_packages(include=['tempsdb', 'tempsdb.*']),
       install_requires=['satella>=2.14.24', 'ujson'],
       ext_modules=ext_modules,
